47d9fd255a2a43a45a4f4b9aaa3e9691dbedad34
Denchikcool <kuzmenok.den@yandex.ru>
2026-05-17 19:15:28 +0700

    Добавлена генерация config.xml - шаблон конфигурации по UML-модели

--- generators.py
+++ generators.py
@@ -1,5 +1,29 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+def build_xml_element(class_name: str, model: dict, children_map: dict) -> ET.Element:
+    elem = ET.Element(class_name)
+    cls = model["classes"][class_name]
+
+    for attr in cls["attributes"]:
+        sub = ET.SubElement(elem, attr["name"])
+        sub.text = attr["type"]
+    
+    for child_name, _ in children_map.get(class_name, []):
+        elem.append(build_xml_element(child_name, model, children_map))
+    
+    return elem
+
 def generate_config_xml(model: dict, children_map: dict) -> str:
-    raise NotImplementedError
+    root_class = next(name for name, info in model["classes"].items() if info["isRoot"])
+    root_elem = build_xml_element(root_class, model, children_map)
+
+    raw = ET.tostring(root_elem, encoding="unicode")
+    dom = minidom.parseString(raw)
+    pretty = dom.toprettyxml(indent="   ")
+    lines = [l for l in pretty.splitlines() if l.strip() and not l.startswith("<?xml")]
+
+    return "\n".join(lines) + "\n"
 
 def generate_meta_json(model: dict, children_map: dict) -> list:
     raise NotImplementedError
