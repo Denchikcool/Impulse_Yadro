5c459f70e61dfd086c463544572fd06b97b197f8
Denchikcool <kuzmenok.den@yandex.ru>
2026-05-18 20:40:44 +0700

    Рефакторинг кода, минимальные сообщения в терминал о статусе выполнения программы, валидация res_patched_config.json и patched_config.json

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ def write_json(path: str, data) -> None:
 def main():
     base = os.path.dirname(os.path.abspath(__file__))
 
-    #xml_path = os.path.join(base, "impulse_test_input.xml")
-    #config_path = os.path.join(base, "config.json")
-    #patched_path = os.path.join(base, "patched_config.json")
-
     model = parse_xml(os.path.join(base, "impulse_test_input.xml"))
     children_map = build_children_map(model)
 
@@ -34,22 +30,14 @@ def main():
     with open(os.path.join(base, "config.xml"), "w", encoding="utf-8") as f:
         f.write(config_xml)
     print("[OK] config.xml")
-
-    #meta = generate_meta_json(model, children_map)
-    #with open(os.path.join(base, "meta.json"), "w", encoding="utf-8") as f:
-        #json.dump(meta, f, ensure_ascii=False, indent=4)
     
     write_json(os.path.join(base, "meta.json"), generate_meta_json(model, children_map))
 
     delta = generate_delta(config, patched)
-    #with open(os.path.join(base, "delta.json"), "w", encoding="utf-8") as f:
-        #json.dump(delta, f, ensure_ascii=False, indent=4)
 
     write_json(os.path.join(base, "delta.json"), delta)
 
     result = apply_delta(config, delta)
-    #with open(os.path.join(base, "res_patched_config.json"), "w", encoding="utf-8") as f:
-        #json.dump(result, f, ensure_ascii=False, indent=4)
     
     write_json(os.path.join(base, "res_patched_config.json"), result)
 
@@ -59,6 +47,6 @@ def main():
         extra = set(result) - set(patched)
         missing = set(patched) - set(result)
         print(f"\n[WARNING] Расхождение: лишние ключи = {extra}, отсутствующие = {missing}")
-        
+
 if __name__ == "__main__":
     main()
